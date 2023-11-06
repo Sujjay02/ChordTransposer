f37326a8587bfc3f62828e05d052a5122801d9a9
Sujjay02 <63737819+Sujjay02@users.noreply.github.com>
2023-11-05 21:41:12 -0500

    Update ChordRunner.java

--- ChordRunner.java
+++ ChordRunner.java
@@ -1,1 +1,26 @@
+import java.util.*;
 
+public class ChordRunner {
+  public static void main(String{}args){
+    Scanner input = New Scanner (System.in);
+    System.out.println("Choose a song to access. Type the name of the song [ex: Here I am to Worship]"); 
+    string song = input.next();
+  
+  static{
+    try {
+      Scanner input = new Scanner(new File(song.toLowerCase()+".csv"));
+      while(input.hasNextLine()){
+        String[] temp = input.nextLine().split(" ");
+        sentiment.put(temp[0],Double.parseDouble(temp[1]));
+      }
+      input.close();
+    }
+    catch(Exception e){
+      System.out.println("This song is not part of the database yet");
+    }
+
+    }
+  }
+
+}
+    
